47db9c4ae11c3391bc9f0c7fa189a2e229d9473b
Shane Bell <being.for.itself@gmail.com>
2019-12-14 22:00:58 -0500

    And more prototyping

--- source/ship/ship.py
+++ source/ship/ship.py
@@ -1,2 +1,13 @@
-def Vessel(object):
-    pass
+class Vessel(object):
+    def __init__(self):
+        self.max_hp = 1
+        self.hp = self.max_hp
+        self.speed = 1
+        self.range = 1
+        self.damage = 1
+        self.size = 1, 1
+        self.pos = 0, 0
+        self.has_moved = False
+
+    def fight(self, other):
+        other.hp -= self.damage
