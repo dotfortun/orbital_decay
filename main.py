0724722a2941ade25f3397ff447c6c074ca8cd9e
Shane Bell <being.for.itself@gmail.com>
2020-01-23 21:06:55 -0500

    Switching back to command line testing.

--- main.py
+++ main.py
@@ -1,42 +1,34 @@
-from flask import Flask
+from game.fleet.fleet import Fleet
+from game.grid.grid import Grid
+from game.ships.bomber import Bomber
+from game.ships.destroyer import Destroyer
+from game.ships.fighter import Fighter
 
-srv = Flask(__name__)
+from copy import deepcopy
 
-@srv.route('/')
-def hello_world():
-    return "Hello, world!"
+red = Fleet()
+red.name = "Red"
+blu = Fleet()
+blu.name = "Blu"
 
-# from game.fleet.fleet import Fleet
-# from game.grid.grid import Grid
-# from game.ships.bomber import Bomber
-# from game.ships.destroyer import Destroyer
-# from game.ships.fighter import Fighter
-#
-# from copy import deepcopy
-#
-# red = Fleet()
-# red.name = "Red"
-# blu = Fleet()
-# blu.name = "Blu"
-#
-# red.ships.append(Destroyer())
-# blu.ships = [
-#     Fighter(),
-#     Fighter(),
-#     Bomber(),
-#     Fighter(),
-#     Fighter(),
-#     Bomber(),
-#     Fighter(),
-#     Bomber(),
-#     Fighter(),
-#     Fighter()
-# ]
-#
-# for idx, ship in enumerate(blu.ships):
-#     ship.move(idx, 5)
-# for ship in red.ships:
-#     ship.move(5, 2)
-#
-# grid = Grid(red, blu)
-# print(grid)
+red.ships.append(Destroyer())
+blu.ships = [
+    Fighter(),
+    Fighter(),
+    Bomber(),
+    Fighter(),
+    Fighter(),
+    Bomber(),
+    Fighter(),
+    Bomber(),
+    Fighter(),
+    Fighter()
+]
+
+for idx, ship in enumerate(blu.ships):
+    ship.move(idx, 5)
+for ship in red.ships:
+    ship.move(5, 2)
+
+grid = Grid(red, blu)
+print(grid)
--- server/server.py
+++ server/server.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 srv = Flask(__name__)
 
+
 @srv.route('/')
 def hello_world():
     return "Hello, world!"
